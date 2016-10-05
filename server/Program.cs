106dd3aff9af8f07799e0505b142b370e8ab695c
Keiko Tamate <Keiko Tamate>
2016-10-05 18:02:51 -0400

    Initial Commit.

---
+++ server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Initializing Server... ");
+
+            string port = null;
+            string path = null;
+            if (args.Length > 1) { port = args[0]; }
+            if (args.Length > 1) { path = args[1]; }
+
+            if (args.Length > 1)
+            {
+                if (args[0] == "--port"){ port = args[1]; }
+                if (args[0] == "--path"){ path = args[1]; }
+            }
+
+            if (args.Length > 3)
+            {
+                if (args[2] == "--port"){ port = args[3]; }
+                if (args[2] == "--path"){ path = args[3]; }
+            }
+
+            Server server = new Server(port, path);
+
+            Console.WriteLine("Starting Thread... ");
+
+            Thread thread = new Thread(new ThreadStart(server.listen));
+            thread.Start();
+        }
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace server
+{
+    class Server
+    {
+        int port;
+        string rootpath;
+        const int defaultPortNumber = 80;
+        const string backslash = @"\";
+        TcpListener server;
+        TcpClient client;
+
+        public Server()
+        {
+            port = defaultPortNumber;
+            rootpath = Directory.GetCurrentDirectory();
+            notifyInitReport();
+        }
+
+        public Server(int port, string path)
+        {
+            if (isPortAvailable(port))
+            {
+                this.port = port;
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Port Not Available.");
+                Console.WriteLine($"Default Port Number {defaultPortNumber} will be used instead.");
+                Console.WriteLine();
+                this.port = defaultPortNumber;
+            }
+            assignRootPath(path);
+            notifyInitReport();
+        }
+
+        public Server(string port, string path)
+        {
+            int portNum;
+            if (int.TryParse(port, out portNum))
+            {
+                if (isPortAvailable(portNum))
+                {
+                    this.port = portNum;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Port Not Available.");
+                    Console.WriteLine($"Default Port Number {defaultPortNumber} will be used instead.");
+                    Console.WriteLine();
+                    this.port = defaultPortNumber;
+                }
+            }
+            else
+            {
+                if (port != null)
+                {
+                    Console.WriteLine($"ERROR: Port parameter provided is not a number.");
+                    Console.WriteLine($"Default Port Number {defaultPortNumber} will be used instead.");
+                    Console.WriteLine();
+                }
+                this.port = defaultPortNumber;
+            }
+            assignRootPath(path);
+            notifyInitReport();
+        }
+
+        private bool isPortAvailable(int port)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.LocalEndPoint.Port == port)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void assignRootPath(string path)
+        {
+            if (path != null)
+            {
+                rootpath = path;
+            }
+            else
+            {
+                rootpath = Directory.GetCurrentDirectory();
+            }
+        }
+
+        public void notifyInitReport()
+        {
+            Console.WriteLine("Server initialization Complete!");
+            Console.WriteLine($"Using PORT : {port}");
+            Console.WriteLine($"Using PATH : {rootpath}");
+            Console.WriteLine();
+        }
+
+        public void listen()
+        {
+            server = new TcpListener(IPAddress.Loopback, port); // Port number in localhost
+            server.Start();
+            Console.WriteLine($"Listening on port {port}...");
+            Console.WriteLine();
+
+            // Loop for listening clients
+
+            //while (true)
+            //{
+            //    // Accepting connection with new client
+            //    client = server.AcceptTcpClient();
+            //    // Handling request from client in a separate thread
+            //    //Thread thread = new Thread(new ThreadStart());
+            //    //thread.Start();
+            //    //Thread.Sleep(100);
+            //}
+        }
+    }
+}
