2f79c417982985b030d785be744049291615cf46
Keiko Tamate <Keiko Tamate>
2016-10-05 19:02:09 -0400

    Port and path validations added.

--- server/Program.cs
+++ server/Program.cs
@@ -5,15 +5,14 @@ namespace server
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.WriteLine();
             Console.WriteLine("Initializing Server... ");
 
             string port = null;
             string path = null;
-            if (args.Length > 1) { port = args[0]; }
-            if (args.Length > 1) { path = args[1]; }
+            
 
             if (args.Length > 1)
             {
@@ -27,12 +26,26 @@ namespace server
                 if (args[2] == "--path"){ path = args[3]; }
             }
 
-            Server server = new Server(port, path);
+
+            Server server;
+
+            try
+            {
+                server = new Server(port, path);
+            }
+            catch
+            {
+                //Console.ReadLine();
+                return 1;
+            }
+            
 
             Console.WriteLine("Starting Thread... ");
 
             Thread thread = new Thread(new ThreadStart(server.listen));
             thread.Start();
+
+            return 0;
         }
     }
 }
