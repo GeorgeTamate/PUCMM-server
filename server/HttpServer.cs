b1bf17599740e21f36aafc64715af2a3738ba689
GeorgeTamate <georgetamate@gmail.com>
2016-10-10 18:24:01 -0400

    HttpServer and HttpServerState classes added.

---
+++ server/HttpServer.cs
@@ -0,0 +1,14 @@
+﻿
+namespace server
+{
+    public class HttpServer
+    {
+        HttpServerState State;
+
+        public void Start()
+        {}
+
+        public void Stop()
+        {}
+    }
+}
