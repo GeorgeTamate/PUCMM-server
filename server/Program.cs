7b4d69f62ee8852ff42dd4458025abc2d4ff7029
GeorgeTamate <georgetamate@gmail.com>
2016-10-18 17:47:18 -0400

    Task of Assignment 6 complete. Removed intermediate Server.cs class, all its functionality passed to Main.

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace server
 {
@@ -7,32 +9,80 @@ namespace server
         static int Main(string[] args)
         {
             Console.WriteLine();
-            Console.WriteLine("Initializing Server... ");
-
-            bool errorfree;
+            Console.WriteLine("Initializing Server App... ");
+            
+            int portNum;
             string port = null;
             string path = null;
 
-
-
+            // Getting args
             if (args.Length > 1)
             {
-                if (args[0] == "--port"){ port = args[1]; }
-                if (args[0] == "--path"){ path = args[1]; }
+                if (args[0] == "--port") { port = args[1]; }
+                if (args[0] == "--path") { path = args[1]; }
             }
 
             if (args.Length > 3)
             {
-                if (args[2] == "--port"){ port = args[3]; }
-                if (args[2] == "--path"){ path = args[3]; }
+                if (args[2] == "--port") { port = args[3]; }
+                if (args[2] == "--path") { path = args[3]; }
             }
 
-            Server server = new Server();
-            errorfree = server.serverInit(port, path);
-            if (!errorfree) { return 1; }
-            
-            errorfree = server.listen();
-            if (!errorfree) { return 1; }
+            // Validating Port
+            if (!int.TryParse(port, out portNum))
+            {
+                if (port != null)
+                {
+                    Console.WriteLine("ERROR: Port parameter provided is not a integer number.");
+                    return 1;
+                }
+                portNum = 8081;
+            }
+
+            // Validating Path
+            if (path != null)
+            {
+                if (!Directory.Exists(path))
+                {
+                    Console.WriteLine("The path \"{0}\" does not exist.", path);
+                    return 1;
+                }
+            }
+            else
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+
+            // Notifying App initialization
+            Console.WriteLine("Server App initialization Complete!");
+            Console.WriteLine($"Using PORT : {portNum}");
+            Console.WriteLine($"Using PATH : {path}");
+            Console.WriteLine();
+
+            //--------------- Listen --------------//
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string cmd;
+
+            Console.WriteLine($"Listening on port {portNum}...");
+
+            HttpServer httpServer = new HttpServer(portNum);
+            httpServer.Start();
+
+            while (true)
+            {
+                cmd = Console.ReadLine();
+                if (cmd.ToLower() == "uptime")
+                {
+                    Console.WriteLine("Time since Server started: " + stopWatch.Elapsed);
+                }
+                else if (cmd.ToLower() == "break")
+                { break; }
+            };
+
+            httpServer.Stop();
+            stopWatch.Stop();
 
             return 0;
         }
